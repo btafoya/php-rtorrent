0ce54811e8d16723701ea58526b8f9991d219c3d
Lars Bo Rasmussen <lars@k0ngehuset.dk>
2012-03-28 18:11:26 +0200

    Added a loadURL and removed loadStarted and LoadStopped

--- rtorrent.class.php
+++ rtorrent.class.php
@@ -2,18 +2,17 @@
 require("xmlrpc.class.php");
 class rTorrent extends rpcClient {
 	private $_downloadscache = false;
-	function loadStarted($string) {
-		 var_dump($this->request("load.start", array($string)));
-	}
-	function loadStopped($string) {
-		 var_dump($this->request("load", array($string)));
-	}
-	function addTorrent($s) {
-		return $this->loadStarted($s);
+	function loadURL($string,$start = false) {
+		if ($start) { 
+			var_dump($this->request("load_start", array($string)));
+		} else {
+			var_dump($this->request("load", array($string)));
+		}
 	}
 	function getDownloads($view = "") {
 		$download_arr = array();
-		$downloads = $this->multirequest("d.multicall",array($view,"d.get_base_filename=","d.get_name=","d.get_base_path=","d.get_complete=","d.get_hash=","d.get_local_id="));
+		//$downloads = $this->multirequest("d.multicall",array($view,"d.get_base_filename=","d.get_name=","d.get_base_path=","d.get_complete=","d.get_hash=","d.get_local_id="));
+		$downloads = $this->multirequest("d.multicall",array($view,"d.get_base_filename=","d.get_base_path=","d.get_bytes_done=","d.get_chunk_size=","d.get_chunks_hashed=","d.get_complete=","d.get_completed_bytes=","d.get_completed_chunks=","d.get_connection_current=","d.get_connection_leech=","d.get_connection_seed=","d.get_creation_date=","d.get_directory=","d.get_down_rate=","d.get_down_total=","d.get_free_diskspace=","d.get_hash=","d.get_hashing=","d.get_ignore_commands=","d.get_left_bytes=","d.get_local_id=","d.get_local_id_html=","d.get_max_file_size=","d.get_message=","d.get_peers_min=","d.get_name=","d.get_peer_exchange=","d.get_peers_accounted=","d.get_peers_complete=","d.get_peers_connected=","d.get_peers_max=","d.get_peers_not_connected=","d.get_priority=","d.get_priority_str=","d.get_ratio=","d.get_size_bytes=","d.get_size_chunks=","d.get_size_files=","d.get_skip_rate=","d.get_skip_total=","d.get_state=","d.get_state_changed=","d.get_tied_to_file=","d.get_tracker_focus=","d.get_tracker_numwant=","d.get_tracker_size=","d.get_up_rate=","d.get_up_total=","d.get_uploads_max=","d.is_active=","d.is_hash_checked=","d.is_hash_checking=","d.is_multi_file=","d.is_open=","d.is_private="));
 		foreach ($downloads as $download) {
 			$download_arr[] = new rTorrentDownload($this,$download);
 		}
